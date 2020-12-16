4394092f4d4259df3b9b6b3f897c7227dfdf2fc8
Andrei2832 <stasenko-andrei2002@mail.ru>
2020-12-16 18:04:46 +0300

    Авторизация Тест

--- app/src/main/java/com/example/ksinfo/LoginActivity.java
+++ app/src/main/java/com/example/ksinfo/LoginActivity.java
@@ -6,35 +6,50 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+public class LoginActivity extends AppCompatActivity {
 
     TextView LoginText;
     TextView PasswordText;
+    private RelativeLayout activity_login;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_login);
 
         LoginText = findViewById(R.id.LoginText);
         PasswordText = findViewById(R.id.PasswordText);
+        activity_login = findViewById(R.id.activity_login);
 
         Button loginButton = findViewById(R.id.LoginButton);
         Button guestButton = findViewById(R.id.NoRegistrationButton);
+
+        mAuth = FirebaseAuth.getInstance();
 
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                signIn(LoginText.getText().toString(), PasswordText.getText().toString());
 
-                //TODO логин с данными из бд и проверками
+//                Snackbar.make(activity_login, "Пользователь не найден", Snackbar.LENGTH_LONG).show();
 
                 ((GlobalApplication) getApplication()).setLoginStatus("User");
                 Intent intent = new Intent(LoginActivity.this,ProfileActivity.class);
@@ -47,26 +62,34 @@ public class LoginActivity extends AppCompatActivity {
                     ((GlobalApplication) getApplication()).setLoginStatus("Admin");
                 }
 
-
                 startActivity(intent);
 
             }
         });
-
-
 
         guestButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 ((GlobalApplication) getApplication()).setLoginStatus("Guest");
-                Intent intent = new Intent(LoginActivity.this,ProfileActivity.class);
+                Intent intent = new Intent(LoginActivity.this, ProfileActivity.class);
                 startActivity(intent);
             }
         });
-
-
-
-
     }
+
+        public void signIn(String email , String password)
+        {
+            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()) {
+                        Toast.makeText(LoginActivity.this, "Aвторизация успешна", Toast.LENGTH_SHORT).show();
+                    }else
+                        Toast.makeText(LoginActivity.this, "Aвторизация провалена", Toast.LENGTH_SHORT).show();
+
+                }
+            });
+        }
+        
 
 }
---
+++ app/src/main/java/com/example/ksinfo/User.java
@@ -0,0 +1,17 @@
+package com.example.ksinfo;
+
+public class User {
+
+    String email;
+    String password;
+
+    public User(){
+
+    }
+
+    public  User(String email, String password){
+        this.email = email;
+        this.password = password;
+    }
+
+}
