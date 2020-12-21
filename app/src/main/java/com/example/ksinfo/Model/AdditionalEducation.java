49e3c0b55b95f0b3da493fc49e1760235110eebd
Andrei2832 <stasenko-andrei2002@mail.ru>
2020-12-21 17:27:13 +0300

    Загруска данных (кружки(new))

---
+++ app/src/main/java/com/example/ksinfo/Model/AdditionalEducation.java
@@ -0,0 +1,30 @@
+package com.example.ksinfo.Model;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+import java.io.Serializable;
+
+@IgnoreExtraProperties
+public class AdditionalEducation implements Serializable {
+    public String id;
+    public String place;
+    public String registration;
+    public String status;
+    public String teacher;
+    public String text;
+    public String title;
+
+    public AdditionalEducation() {
+    }
+
+    public AdditionalEducation(String id, String place, String registration, String status, String teacher, String text, String title) {
+        this.id = id;
+        this.place = place;
+        this.registration = registration;
+        this.status = status;
+        this.teacher = teacher;
+        this.text = text;
+        this.title = title;
+    }
+
+}
