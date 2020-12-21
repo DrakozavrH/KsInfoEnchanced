dacd481d708db685b141a6124b021f301e40fe58
Andrei2832 <stasenko-andrei2002@mail.ru>
2020-12-21 11:31:19 +0300

    Загруска данных (пользователь)

--- app/src/main/java/com/example/ksinfo/LoginActivity.java
+++ app/src/main/java/com/example/ksinfo/LoginActivity.java
@@ -1,12 +1,9 @@
 package com.example.ksinfo;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,20 +11,25 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.ksinfo.Model.User;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
 
+
     TextView LoginText;
     TextView PasswordText;
-    private RelativeLayout activity_login;
     private FirebaseAuth mAuth;
+    private FirebaseDatabase mDataBase;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -36,20 +38,19 @@ public class LoginActivity extends AppCompatActivity {
 
         LoginText = findViewById(R.id.LoginText);
         PasswordText = findViewById(R.id.PasswordText);
-        activity_login = findViewById(R.id.activity_login);
 
         Button loginButton = findViewById(R.id.LoginButton);
         Button guestButton = findViewById(R.id.NoRegistrationButton);
 
         mAuth = FirebaseAuth.getInstance();
-
+        mDataBase = FirebaseDatabase.getInstance();
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 signIn(LoginText.getText().toString(), PasswordText.getText().toString());
 
-                //Snackbar.make(activity_login, "Пользователь не найден", Snackbar.LENGTH_LONG).show();
+//                Snackbar.make(activity_login, "Пользователь не найден", Snackbar.LENGTH_LONG).show();
 
                 ((GlobalApplication) getApplication()).setLoginStatus("User");
                 Intent intent = new Intent(LoginActivity.this,ProfileActivity.class);
@@ -79,13 +80,49 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
-        public void signIn(String email , String password)
+        public void signIn(final String email , String password)
         {
             mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
                     if(task.isSuccessful()) {
+
                         Toast.makeText(LoginActivity.this, "Aвторизация успешна", Toast.LENGTH_SHORT).show();
+
+                        final DatabaseReference UserRef = mDataBase.getReference("Users");
+                        UserRef.orderByChild("email").equalTo(LoginText.getText().toString()).addChildEventListener(new ChildEventListener() {
+
+                            @Override
+                            public void onChildAdded( DataSnapshot snapshot, String previousChildName) {
+                                User user = snapshot.getValue(User.class);
+                                LoginText.setText(user.Name);
+
+                            }
+                            @Override
+                            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                            }
+                            @Override
+                            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+                            }
+                            @Override
+                            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                            }
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+                            }
+                        });
+
+
+                        ((GlobalApplication) getApplication()).setLoginStatus("User");
+                        Intent intent = new Intent(LoginActivity.this,ProfileActivity.class);
+                        String name = LoginText.getText().toString();
+                        intent.putExtra("name", name);
+
+                        if(LoginText.getText().toString().equals("admin") && PasswordText.getText().toString().equals("admin")){
+                            ((GlobalApplication) getApplication()).setLoginStatus("Admin");
+                        }
+
+                        startActivity(intent);
                     }else
                         Toast.makeText(LoginActivity.this, "Aвторизация провалена", Toast.LENGTH_SHORT).show();
 
---
+++ app/src/main/java/com/example/ksinfo/Model/User.java
@@ -0,0 +1,30 @@
+package com.example.ksinfo.Model;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+import java.io.Serializable;
+
+@IgnoreExtraProperties
+public class User implements Serializable {
+
+    public String email;
+    public String Name;
+    public String Patronymic;
+    public String Surname;
+    public String group;
+    public int role;
+
+    public User(){
+
+    }
+
+    public  User(String Name, String Patronymic, String Surname, String email, String group, int role){
+        this.Name = Name;
+        this.Patronymic = Patronymic;
+        this.Surname = Surname;
+        this.email = email;
+        this.group = group;
+        this.role = role;
+    }
+
+}
